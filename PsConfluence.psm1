2e4ab0470097263174bd7e575f46b660a35a3b40
Oliver Lipkau <oliver@lipkau.net>
2015-07-21 11:18:10 +0200

    Fixed loading of module

--- PsConfluence.psm1
+++ PsConfluence.psm1
@@ -1,11 +1,9 @@
-﻿#Requires -Version 3.0
-<#
+﻿<#
     .Synopsis
-        This is a compilation of functions to simplify scripts to manage AD tasks
+        This module facilitates the interaction with Atlassian's Confluence using the APIs
         
     .Description
-        This compilation contains functions to simplify the management of Active Directories.
-        Check the examples to see how to find computers, create new users, and much more.
+        This module facilitates the interaction with Atlassian's Confluence using the APIs
     
     .Notes
         Author   : Oliver Lipkau <oliver@lipkau.net>
@@ -14,18 +12,6 @@
             2015/07/06             Created
         
 #>
-param()
 
-$ScriptPath = $MyInvocation.MyCommand.Path
-$PsCOnfluenceModuleHome = split-path -parent $ScriptPath
-
-$functions = Get-ChildItem "$PsCOnfluenceModuleHome\" *.ps1 -Recurse
-$functions | foreach-object -begin {$i=0} -process `
-{
-    $name = $_.name.Replace(".ps1","")
-    $path = $_.fullname
-    . "$path"
-    $i++
-    Write-Progress -Activity "Loading Module" -Status $name -PercentComplete ($i/$functions.count*100)
-    Start-Sleep -Milliseconds 200
-}
+Get-ChildItem -Path $PSScriptRoot -recurse | Unblock-File
+Get-ChildItem -Path $PSScriptRoot\*.ps1 -recurse | Foreach-Object{ . $_.FullName }
