ec059cc2b3d7ce100b4e677700b3190541ff3a90
Oliver Lipkau <oliver@lipkau.net>
2015-08-13 09:59:44 +0200

    Explicit declaration of functions and script to avoid injection

--- PsConfluence.psm1
+++ PsConfluence.psm1
@@ -1,17 +1,49 @@
 ﻿<#
     .Synopsis
         This module facilitates the interaction with Atlassian's Confluence using the APIs
-        
+
     .Description
         This module facilitates the interaction with Atlassian's Confluence using the APIs
-    
+
     .Notes
         Author   : Oliver Lipkau <oliver@lipkau.net>
-        
+
         ChangeLog:
             2015/07/06             Created
-        
+
+    .Documentation
+        https://developer.atlassian.com/confdev/confluence-rest-api/confluence-xml-rpc-and-soap-apis/remote-confluence-methods
+        http://www.k15t.com/display/SUPPORT/Changing+all+pages+in+a+space+to+unversioned+pages
+
 #>
 
+# Unblock files (if downloaded from ZIP)
 Get-ChildItem -Path $PSScriptRoot -recurse | Unblock-File
-Get-ChildItem -Path $PSScriptRoot\*.ps1 -recurse | Foreach-Object{ . $_.FullName }
+
+# Import Structures
+. "$PSScriptRoot\Import-Types.ps1"
+
+# Import each file individualy to avoid injections
+#XML-RPC
+$XMLRPCfiles = @('Administration.ps1', `
+                 'Attachement.ps1', `
+                 'Authentication.ps1', `
+                 'Blog.ps1', `
+                 'General.ps1', `
+                 'Labels.ps1', `
+                 'Notifications.ps1', `
+                 # 'Pages.ps1', `
+                 'Pages_usingObjects.ps1', `
+                 'Permissions.ps1', `
+                 'Search.ps1', `
+                 'Spaces.ps1', `
+                 'UserManagement.ps1')
+foreach ($file in $XMLRPCfiles)
+    {. "$PSScriptRoot\XML-RPC\$file"}
+
+##REST
+#$RESTfiles = @('Authentication.ps1', `
+#               'Pages.ps1', `
+#               'Scroll.ps1')
+#foreach ($file in $RESTfiles)
+#    {. "$PSScriptRoot\REST\$file"}
