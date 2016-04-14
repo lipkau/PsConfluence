609e7fb5c840db0b1ee672b269ae23cde27df9c7
Oliver Lipkau <oliver@lipkau.net>
2016-04-14 10:36:48 +0200

    Implemented new authentication method

--- XML-RPC/Authentication.ps1
+++ XML-RPC/Authentication.ps1
@@ -2,37 +2,38 @@
 <#  Authentications #>
 function Connect-Confluence {
     <#
-    .SYNOPSIS
-        .
+        .SYNOPSIS
+            Authenticate with a Confluence SOAP or REST API.
 
-    .DESCRIPTION
-        .
+        .DESCRIPTION
+            Log in a user.
+            Returns a String authentication token to be passed as authentication to all other remote calls.
+            Must be called before any other method in a remote conversation. From 1.3 onwards, you can supply an empty string as the token to be treated as being the anonymous user.
 
-    .NOTES
-        AUTHOR : Oliver Lipkau <oliver@lipkau.net>
-        VERSION: 1.0.0 - OL - Replaced hashtables with Objects
+        .NOTES
+            AUTHOR : Oliver Lipkau <oliver@lipkau.net>
+            VERSION: 1.0.0 - OL - initial Code
 
-    .INPUTS
-        string
-        System.Management.Automation.PSCredential
+        .INPUTS
+            string
+            System.Management.Automation.PSCredential
 
-    .OUTPUTS
-        string
-        Microsoft.PowerShell.Commands.HtmlWebResponseObject
+        .OUTPUTS
+            string
+            Microsoft.PowerShell.Commands.HtmlWebResponseObject
 
-    .EXAMPLE
-        Invoke-ConfluenceLogin -apiURi "http://example.com" -Credential (Get-Credential)
-        -----------
-        Description
-        Log in to Confluence
+        .EXAMPLE
+            Invoke-ConfluenceLogin -apiURi "http://example.com" -Credential (Get-Credential)
+            -----------
+            Description
+            Log in to Confluence
 
-    .LINK
-        Atlassians's Docs:
-            String login(String username, String password)
+        .LINK
+            Atlassians's Docs:
+                String login(String username, String password)
 
     #>
-    [CmdletBinding(
-    )]
+    [CmdletBinding()]
     [OutputType(
         [string],
         [Microsoft.PowerShell.Commands.HtmlWebResponseObject]
@@ -52,7 +53,7 @@ function Connect-Confluence {
         )]
         [System.Management.Automation.PSCredential]$Credential,
 
-        #
+        # API Connection type. Can be SOAP (XML-RPC) or REST.
         [ValidateSet('rest','xmlrpc')]
         [Parameter(
             Position=2,
@@ -61,6 +62,8 @@ function Connect-Confluence {
         [Alias('Type')]
         $ConnectionType,
 
+        # Authentication Mode. Can be OAuth or Basic.
+        # OAuth will return a WebSession Instance.
         [ValidateSet('OAuth', "Basic")]
         [Parameter(
             Mandatory=$false
@@ -82,11 +85,11 @@ function Connect-Confluence {
             "rest" {
                 if ($AuthenticationMethod -eq "OAuth")
                 {
-                    $global:response = Invoke-WebRequest $apiURi -SessionVariable cf
+                    $response = Invoke-WebRequest $apiURi -SessionVariable cf
                     $form = $response.Forms | Where-Object {$_.Fields.Keys -contains $formFields.user -and $_.Fields.Keys -contains $formFields.password}
                     $form.Fields[$formFields.user] = $Credential.UserName
                     $form.Fields[$formFields.password] = ($Credential.getnetworkcredential().password)
-                    $response = Invoke-WebRequest -Uri ($apiURi + "" + $form.Action) -WebSession $cf -Method POST -Body $form.Fields
+                    Invoke-WebRequest -Uri ($apiURi + "" + $form.Action) -WebSession $cf -Method POST -Body $form.Fields | Out-Null
 
                     Set-ConfluenceEndpoint -apiURi $apiURi -WebSession $cf
                     return $cf
@@ -98,10 +101,15 @@ function Connect-Confluence {
                 break
             }
             "xmlrpc" {
-                $authToken = Invoke-ConfluenceCall -apiURi $apiURi -MethodName "confluence2.login" -Params ([string]($Credential.UserName),[string]($Credential.getnetworkcredential().password))
+                $ConfluenceAuthToken = Invoke-ConfluenceCall -apiURi $apiURi -MethodName "confluence2.login" -Params ([string]($Credential.UserName),[string]($Credential.getnetworkcredential().password))
+                $PDFExportAuthToken = Invoke-ConfluenceCall -apiURi $apiURi -MethodName "pdfexport.login" -Params ([string]($Credential.UserName),[string]($Credential.getnetworkcredential().password))
 
-                Set-ConfluenceEndpoint -apiURi $apiURi -token $authToken
-                return $authToken
+                Set-ConfluenceEndpoint -apiURi $apiURi -token $ConfluenceAuthToken -Method "confluence2"
+                Set-ConfluenceEndpoint -apiURi $apiURi -token $PDFExportAuthToken -Method "pdfexport"
+                New-Object -TypeName PSObject -Prop @{
+                    confluenceToken = $ConfluenceAuthToken
+                    pdfexportToken = $PDFExportAuthToken
+                }
             }
         }
     }
@@ -109,72 +117,7 @@ function Connect-Confluence {
     End
         { Write-Verbose "$($MyInvocation.MyCommand.Name):: Function ended" }
 }
-function Invoke-ConfluenceLogin {
-    <#
-    .SYNOPSIS
-        Authenticate with a Confluence Instance
-
-    .DESCRIPTION
-        Log in a user.
-        Returns a String authentication token to be passed as authentication to all other remote calls.
-        Must be called before any other method in a remote conversation. From 1.3 onwards, you can supply an empty string as the token to be treated as being the anonymous user.
-
-    .NOTES
-        AUTHOR : Oliver Lipkau <oliver@lipkau.net>
-        VERSION: 0.0.1 - OL - Initial Code
-                 1.0.0 - OL - Replaced hashtables with Objects
-
-    .INPUTS
-        string
-        System.Management.Automation.PSCredential
-
-    .OUTPUTS
-        string
-
-    .EXAMPLE
-        Invoke-ConfluenceLogin -apiURi "http://example.com" -Credential (Get-Credential)
-        -----------
-        Description
-        Log in to Confluence
-
-    .LINK
-        Atlassians's Docs:
-            String login(String username, String password)
-
-    #>
-    [CmdletBinding(
-    )]
-    [OutputType(
-        [string]
-    )]
-    param(
-        # The URi of the API interface.
-        [Parameter(
-            Position=0,
-            Mandatory=$true
-        )]
-        [string]$apiURi,
-
-        # Credentials with which to authentication
-        [Parameter(
-            Position=1,
-            Mandatory=$true
-        )]
-        [System.Management.Automation.PSCredential]$Credential
-    )
-
-    Begin
-        { Write-Verbose "$($MyInvocation.MyCommand.Name):: Function started" }
-
-    Process {
-        Write-Verbose "$($MyInvocation.MyCommand.Name):: Authenticate at $apiURi as $($Credential.UserName)"
-        ConvertFrom-Xml (Invoke-ConfluenceCall -Url $apiURi -MethodName "confluence2.login" -Params ([string]($Credential.UserName),[string]($Credential.getnetworkcredential().password)))
-    }
-
-    End
-        { Write-Verbose "$($MyInvocation.MyCommand.Name):: Function ended" }
-}
-function Invoke-ConfluenceLogout {
+function Disconnect-Confluence {
     <#
     .SYNOPSIS
         Log off from Confluence Insatance
@@ -232,71 +175,7 @@ function Invoke-ConfluenceLogout {
     Process {
         Write-Verbose "$($MyInvocation.MyCommand.Name):: Logging out"
         ConvertFrom-Xml (Invoke-ConfluenceCall -Url $apiURi -MethodName "confluence2.logout" -Params ($token))
-    }
-
-    End
-        { Write-Verbose "$($MyInvocation.MyCommand.Name):: Function ended" }
-}
-function Invoke-ConfluencePDFLogin {
-    <#
-    .SYNOPSIS
-        Authenticate with a Confluence Instance for PDF services
-
-    .DESCRIPTION
-        Log in a user.
-        Returns a String authentication token to be passed as authentication to all other remote calls.
-        Must be called before any other method in a remote conversation. From 1.3 onwards, you can supply an empty string as the token to be treated as being the anonymous user.
-
-    .NOTES
-        AUTHOR : Oliver Lipkau <oliver@lipkau.net>
-        VERSION: 0.0.1 - OL - Initial Code
-                 1.0.0 - OL - Replaced hashtables with Objects
-
-    .INPUTS
-        string
-        System.Management.Automation.PSCredential
-
-    .OUTPUTS
-        string
-
-    .EXAMPLE
-        Invoke-ConfluencePDFLogin -apiURi "http://example.com" -Credential (Get-Credential)
-        -----------
-        Description
-        Log in to Confluence
-
-    .LINK
-        Atlassians's Docs:
-            String login(String username, String password)
-
-    #>
-    [CmdletBinding(
-    )]
-    [OutputType(
-        [string]
-    )]
-    param(
-        # The URi of the API interface.
-        [Parameter(
-            Position=0,
-            Mandatory=$true
-        )]
-        [string]$apiURi,
-
-        # Credentials with which to authentication
-        [Parameter(
-            Position=1,
-            Mandatory=$true
-        )]
-        [System.Management.Automation.PSCredential]$Credential
-    )
-
-    Begin
-        { Write-Verbose "$($MyInvocation.MyCommand.Name):: Function started" }
-
-    Process {
-        Write-Verbose "$($MyInvocation.MyCommand.Name):: Authenticate at $apiURi as $($Credential.UserName)"
-        ConvertFrom-Xml (Invoke-ConfluenceCall -Url $apiURi -MethodName "pdfexport.login" -Params ([string]($Credential.UserName),[string]($Credential.getnetworkcredential().password)))
+        ConvertFrom-Xml (Invoke-ConfluenceCall -Url $apiURi -MethodName "pdfexport.logout" -Params ($token))
     }
 
     End
